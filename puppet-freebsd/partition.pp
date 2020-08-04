f75d42cd4290363f4b95fc08006d7f374ccb11a2
Mohammad Khabir Uddin <masumndc1@gmail.com>
2020-08-04 18:46:33 +0300

    puppet-freebsd: we dont need this here
    we will install all manifest from a module called
    puppet-freebsd by r10k.

--- puppet-freebsd/partition.pp
+++
@@ -1,4 +0,0 @@
-# checking partition and their size
-each ( $facts['partitions'] ) |$name, $capacity | {
-  notice("partition ${name} has size of ${capacity['size']}")
-}
--- puppet-freebsd/upgrade.pp
+++
@@ -1,9 +0,0 @@
-#updrading freebsd daily, nightly
-schedule { 'nightly':
-  period => 'daily',
-  range  => '21 - 22',
-}
-
-exec { '/usr/sbin/pkg upgrade -y':
-  schedule => 'nightly',
-}

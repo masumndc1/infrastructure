0eb94dd0494f113c3b58a9dbc687600704e1ad65
Mohammad Khabir Uddin <masumndc1@gmail.com>
2020-08-16 17:57:32 +0300

    lxd bootstrap shell file

---
+++ lxd_bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo -e "Installing python"
+yum install -y python || apt-get install -y python
+echo -e "downloading ansible python installer"
+curl -O https://raw.githubusercontent.com/masumndc1/infrastructure/master/ansible_install.py
+echo -e "Installing ansible"
+python ansible_install.py
+echo -e "Adding user, public key, installing sudo and openssh-server and running it"
+curl -O https://raw.githubusercontent.com/masumndc1/infrastructure/master/pkg_srv.yml 
+ansible-playbook pkg_srv.yml

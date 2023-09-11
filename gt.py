9495b8dbec25ca96b69b7e2e33623b16a5b889fd
khabiruddin <khabir.uddin@csc.fi>
2023-09-11 23:42:03 +0300

    new gt.py file

--- gt.py
+++ gt.py
@@ -1,43 +1,49 @@
 #!/usr/bin/env python3
 # this is a simple python script to automate
-# git pull, commit and push
-# 
+# git pull, commit and push to github.
 
 import os
+import shutil
 import sys
-from blessings import Terminal
+import subprocess
+
 
 class GitOperation():
-  global term
-  term = Terminal()
+    '''
+    gt.py "commit_msg" branch_name(optional) default_branch: master
+    '''
 
-  def __init__(self):
-    if len(sys.argv) == 2: 
-      self.msg = sys.argv[1]
-      GitOperation.GitPullPush(self.msg)
+    def __init__(self):
+        self.msg = sys.argv[1]
+        self.branch = sys.argv[2] if len(sys.argv) == 3 else "master"
+        self._add_new_files()
 
-    elif len(sys.argv) < 2: 
-      print(term.red + "%20s" % "You want to pull updates from upstreams. Pulling update" + term.normal)
-      print(term.red + "%20s" % "If you dont want to pull, then usage is:" + term.normal)
-      print(term.red + "%20s" % "python3 gt.py \"commit msg\"" + term.normal)
-      os.system('git pull --rebase')
+    def _term_size(self):
+        col, _ = shutil.get_terminal_size()
+        print("-" * col)
 
-  @classmethod
-  def GitPullPush(cls, msg):
-    GitOperation.GitPrint('*')
-    os.system ('git add .')
-    print(term.blue + "%20s" % "Commiting with msg: " + "'" + msg + "'" + term.cyan)
-    os.system('git commit -m "%s"' % msg) 
-    print(term.red + "%s" % "Pulling before pushing")
-    os.system('git pull --rebase')
-    print(term.green + "%20s" % "Pushing now to upstream " + term.cyan)
-    os.system('git push origin master')
-    GitOperation.GitPrint('-')
+    def _add_new_files(self):
+        self._term_size()
+        retcode = subprocess.call("git add .", shell=True)
+        self._commit() if not retcode else sys.exit("could not add files")
 
-  @classmethod
-  def GitPrint(cls, sym):
-    cls.num = term.width
-    print(term.red + sym * cls.num + term.normal)
+    def _commit(self):
+        retcode = subprocess.call(
+            'git commit -m "%s"' % self.msg, shell=True)
+        self._push() if not retcode else sys.exit("could not commit")
+
+    def _push(self):
+        retcode = subprocess.call(
+            'git push origin "%s"' % self.branch, shell=True)
+        if not retcode:
+            print(f"Pushed to {self.branch} branch")
+            self._term_size()
+        else:
+            sys.exit("could not push")
 
 
-GitOperation()
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        GitOperation()
+    else:
+        print(GitOperation.__doc__)

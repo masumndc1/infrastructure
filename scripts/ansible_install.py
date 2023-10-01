07fd95e354e45e5d36e5565fdb29bcf94df55417
Khabir Uddin <masumndc1@gmail.com>
2023-10-01 20:48:26 +0300

    logic changes in ansible install py script

--- scripts/ansible_install.py
+++ scripts/ansible_install.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Python must be installed before hand. 
+Python must be installed before hand.
 Run OS command to install python first.
 for Freebsd run (pkg install python36) first
 for Ubuntu16  run: sudo python3 ansible_install.py
@@ -12,55 +12,55 @@ for Freebsd12 run: sudo ./ansible_install.py
 import os
 import platform
 
+
 def ansible_install():
-# Ubuntu 
     if 'Linux' in platform.system():
-      if 'Ubuntu' in platform.dist():
-        ubuntu_commands = [ "apt update", 
-            "apt install -y software-properties-common", 
-            "apt-add-repository --yes --update ppa:ansible/ansible",
-            "apt install -y ansible"
-           ]
+        if os.path.exists('/usr/bin/apt'):
+            ubuntu_commands = [
+                "apt update",
+                "apt install -y software-properties-common",
+                "apt-add-repository --yes --update ppa:ansible/ansible",
+                "apt install -y ansible"
+            ]
 
-        for command in ubuntu_commands:
-          os.system(command)
+            for command in ubuntu_commands:
+                os.system(command)
 
-# CentOS
-      elif 'centos' in platform.dist():
-        centos_commands = [ "yum install -y epel-release",
-                          "yum -y update",
-                          "yum install -y ansible" 
-                        ]
+        elif os.path.exists('/usr/bin/yum'):
+            centos_commands = [
+                "yum install -y epel-release",
+                "yum -y update",
+                "yum install -y ansible"
+            ]
 
-        for command in centos_commands:
-          os.system(command)
+            for command in centos_commands:
+                os.system(command)
 
-# FreeBSD
     elif 'FreeBSD' in platform.system():
-      freebsd_commands = ["pkg update -f -q",
-                         "pkg install -y py37-ansible"
-                        ]
-      
-      for command in freebsd_commands:
-         os.system(command)
-      
-# DragonFly
+        freebsd_commands = [
+            "pkg update -f -q",
+            "pkg install -y py37-ansible"
+        ]
+
+        for command in freebsd_commands:
+            os.system(command)
+
     elif 'DragonFly' in platform.system():
-      dragonfly_commands = [ "pkg install -y py37-ansible"
-                         ]
-      
-      for command in dragonfly_commands:
-         os.system(command)
+        dragonfly_commands = [
+            "pkg install -y py37-ansible"
+        ]
 
-# OpenBSD
+        for command in dragonfly_commands:
+            os.system(command)
+
     elif 'OpenBSD' in platform.system():
-      openbsd_commands = [ "pkg_add ansible"
-                       ]
-      
-      for command in openbsd_commands:
-         os.system(command)
+        openbsd_commands = [
+            "pkg_add ansible"
+        ]
 
-    
-ansible_install()
+        for command in openbsd_commands:
+            os.system(command)
 
 
+if __name__ == '__main__()':
+    ansible_install()

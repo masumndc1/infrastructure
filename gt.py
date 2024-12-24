29394eb416798197417aa2c0e81d4941d1a7ca3d
Khabir Uddin <khuddin@csc.fi>
2024-12-24 08:47:21 +0200

    modified gt python scripts

--- gt.py
+++ gt.py
@@ -2,20 +2,19 @@
 # this is a simple python script to automate
 # git pull, commit and push to github.
 
-import os
 import shutil
 import sys
 import subprocess
 
 
-class GitOperation():
-    '''
-    gt.py "commit_msg" branch_name(optional) default_branch: master
-    '''
+class GitOperation:
+    """
+    ./gt.py "commit-msg" master/branch-name
+    """
 
-    def __init__(self):
-        self.msg = sys.argv[1]
-        self.branch = sys.argv[2] if len(sys.argv) == 3 else "master"
+    def __init__(self, msg, branch):
+        self.msg = msg
+        self.branch = branch
         self._add_new_files()
 
     def _term_size(self):
@@ -28,13 +27,11 @@ class GitOperation():
         self._commit() if not retcode else sys.exit("could not add files")
 
     def _commit(self):
-        retcode = subprocess.call(
-            'git commit -m "%s"' % self.msg, shell=True)
+        retcode = subprocess.call('git commit -m "%s"' % self.msg, shell=True)
         self._push() if not retcode else sys.exit("could not commit")
 
     def _push(self):
-        retcode = subprocess.call(
-            'git push origin "%s"' % self.branch, shell=True)
+        retcode = subprocess.call('git push origin "%s"' % self.branch, shell=True)
         if not retcode:
             print(f"Pushed to {self.branch} branch")
             self._term_size()
@@ -42,8 +39,14 @@ class GitOperation():
             sys.exit("could not push")
 
 
-if __name__ == "__main__":
+def main():
     if len(sys.argv) >= 2:
-        GitOperation()
+        msg = sys.argv[1]
+        branch = sys.argv[2] if len(sys.argv) == 3 else "master"
+        GitOperation(msg, branch)
     else:
         print(GitOperation.__doc__)
+
+
+if __name__ == "__main__":
+    main()
